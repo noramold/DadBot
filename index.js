3d39ed6d47b96962735b058016a999705bd75bd8
noramold <noramold@hotmail.no>
2021-07-18 14:31:18 +0100

    if u say hi dad it replyies that ure not dad

--- index.js
+++ index.js
@@ -50,15 +50,23 @@ client.on("message", async message => {
 
             if(sendt.startsWith(search[i].lang)) // || sendt.endsWith(search[1].lang))
             {
-                let word = sendt.replace(search[i].lang,"");
-                let greets = search[i].greeting;
-                let hi_dad = search[i].dad;
-
-                let hi = sendt.replace("i'm","");
-                let hi1 = sendt.replace("i am", "")
+                let word    = sendt.replace(search[i].lang,""); // What people sent with out I'm
+                let greets  = search[i].greeting; // Greeting e.g Hello
+                let hi_dad  = search[i].dad; // I'm dad reply 
+                let no_i_am = search[i].not; // No in different languages
                 
-                if(hi === " dad" || hi1 === " dad")
-                    message.inlineReply("No, I'm dad\n");
+
+                // let hi = sendt.replace("i'm","");
+                // let hi1 = sendt.replace("i am", "")
+                
+                console.log(sendt);
+                console.log(hi_dad + "hi dad");
+                if(sendt === hi_dad)
+                {
+                    console.log("TEEST!!")
+                    message.inlineReply(no_i_am + ", " + hi_dad + "\n");
+                }
+                    // message.inlineReply(me + "," + hi_dad + "\n");
                 else
                     message.inlineReply(greets + word + ", " + hi_dad + "\n");
 
@@ -80,7 +88,8 @@ var search = [
     {
         lang: "ek is", // Afrikaans
         greeting: "Hallo", 
-        dad: "ek is pa"
+        dad: "ek is pa", 
+        not: "Nee"
     }, 
     // {
     //     lang: "أَنَا", // Arabic
@@ -91,76 +100,91 @@ var search = [
         lang: "我是", // Chinese
         greeting: "你好", 
         dad: "我是爸爸", 
+        not: "不"
     },
     {
         lang: "jeg er", // Danish 
         greeting: "Hallo", 
         dad: "jeg er pappa", 
+        not: "Mei"
     },
     {
         lang: "ik ben", // Dutch
         greeting: "Hallo", 
         dad: "ik ben papa", 
+        not: "Nee"
     },
     {
         lang: "mi estas", // Esperanto
         greeting: "Saluton", 
-        dad: "mi estas paĉjo", 
+        dad: "mi estas paĉjo",
+        not: "Ne" 
     },
     {
         lang: "i am", // English
         greeting: "Hello", 
         dad: "i'm dad", 
+        not: "No"
     },
     {
         lang: "i'm", // English
         greeting: "Hi", 
         dad: "i'm dad", 
+        not: "No"
     },
     {
         lang: "je suis", // French 
         greeting: "Salut", 
         dad: "je suis papa", 
+        not: "Non"
     },
     {
         lang: "ich bin", // German
         greeting: "Hallo", 
         dad: "ich bin papa", 
+        not: "Nein"
     },
     {
         lang: "eia au",  // Hawaiian
         greeting: "Aloha", 
-        dad: "ʻO wau ka makuakāne", 
+        dad: "ʻo wau ka makuakāne", 
+        not: "Aʻole", 
     },
     {
         lang: "ég er", // Iclandic
         greeting: "Halló", 
         dad: "ég er pabbi", 
+        not: "Nei"
     },
     {
         lang:  "jeg er", // ["jeg er","eg er","æ e","æg e"], // Norwegian
         greeting: "Hei", 
         dad: "jeg er pappa", 
+        not: "Nei"
     },
     {
         lang: "jestem", // Polish
         greeting: "Cześć", 
         dad: "jestem tatą", 
+        not: "Nie"
     },
     {
         lang: "aniga", // Somali
         greeting: "As-Salam Alaykum", 
         dad: "aniga waxaan ahay aabo", 
+        not: "Maya"
     },
     {
         lang: "yo soy", //Spanish
         greeting: "Hola", 
         dad: "yo soy papa", 
+        not: "No"
     },
     {
         lang: "jag är", // Swedish
         greeting: "Hallo", 
-        dad: "jag är pappa"
+        dad: "jag är pappa", 
+        not: "Nej"
     }, 
 
     /* LAME THING */
